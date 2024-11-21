33727feaaba3b83eacff561d3ed841ec3f326dde
Shahin Sarker <141581883+Shahin4292@users.noreply.github.com>
2024-11-21 14:30:31 +0600

    call api function add done

--- lib/wallpaper.dart
+++ lib/wallpaper.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class Wallpaper extends StatefulWidget {
   const Wallpaper({super.key});
@@ -8,15 +11,36 @@ class Wallpaper extends StatefulWidget {
 }
 
 class _WallpaperState extends State<Wallpaper> {
+  List images = [];
+
+  @override
+  void initState() {
+    fetchApi();
+    super.initState();
+  }
+
+  fetchApi() {
+    http.get(Uri.parse('https://api.pexels.com/v1/curated?per_page=80'),
+        headers: {
+          'Authorization':
+              "PzrwjMFFdssYI6z3sTYrAFpGwUnlnTE0Nft6rnGEsy8O6A0gKvBf6khO"
+        }).then((value) {
+      Map result = jsonDecode(value.body);
+      setState(() {
+        images = result['photos'];
+      });
+      print(images);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
         children: [
           Expanded(
-              child: Container(
             child: GridView.builder(
-                itemCount: 80,
+                itemCount: images.length,
                 gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
                     crossAxisSpacing: 2,
                     mainAxisSpacing: 2,
@@ -25,9 +49,13 @@ class _WallpaperState extends State<Wallpaper> {
                 itemBuilder: (context, index) {
                   return Container(
                     color: Colors.white,
+                    child: Image.network(
+                      images[index]['src']['tiny'],
+                      fit: BoxFit.cover,
+                    ),
                   );
                 }),
-          )),
+          ),
           Container(
             height: 60,
             width: MediaQuery.sizeOf(context).width,
